d9241ebdf6f08b7888e17341dee2d6217835dfc8
Manuel <avillao>
2022-06-20 13:57:59 -0500

    Agregación de nueva ruta para IcosIdos

--- functions/oportCriptoLinks.js
+++ functions/oportCriptoLinks.js
@@ -64,6 +64,57 @@ oportCriptoLinks.get(`/${process.env.API_KEY}/historico`, (req, res) => {
 });
 
 // eslint-disable-next-line max-len
+oportCriptoLinks.get(`/${process.env.API_KEY}/historico/groupByDate`, (req, res) => {
+  const date = new Date();
+  // eslint-disable-next-line max-len
+  const fechaActual = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+  // eslint-disable-next-line max-len
+  const sqlStr = `SELECT * FROM ${process.env.OPORT_CRIPTO_TABLE} WHERE tipo="Op.Hist." AND mostrar_hasta >= ${fechaActual} ORDER BY fecha_publicacion DESC;`;
+  // eslint-disable-next-line max-len
+  pool.getConnection(function(error, connection) {
+    if (error) throw error;
+    connection.query(sqlStr, (err, result, fields) => {
+      const fechasUnicas = [];
+      const dict = [];
+      result.forEach((field) => {
+        if (!(fechasUnicas.includes(JSON.stringify(field.fecha_publicacion)))) {
+          fechasUnicas.push(JSON.stringify(field.fecha_publicacion));
+        }
+      });
+      // eslint-disable-next-line guard-for-in
+      for (const indice in fechasUnicas) {
+        const datosUnicos =[];
+        result.forEach((field)=>{
+          if (fechasUnicas[indice] == JSON.stringify(field.fecha_publicacion)) {
+            datosUnicos.push(field);
+          }
+        });
+        // eslint-disable-next-line max-len
+        dict.push({"fecha": JSON.parse(fechasUnicas[indice]), "datos": datosUnicos});
+      }
+
+      if (result) {
+        connection.release();
+        if (err) throw err;
+        res.status(200).json({
+          status: 200,
+          title: "Historico Agrupado por Fechas en Orden Descendente",
+          data: dict,
+        });
+      } else {
+        connection.release();
+        if (err) throw err;
+        res.status(400).json({
+          status: 400,
+          error: "Request Error",
+          message: "No se encontro Historico",
+        });
+      }
+    });
+  });
+});
+
+// eslint-disable-next-line max-len
 oportCriptoLinks.get(`/${process.env.API_KEY}/tradehold/:transaccion`, (req, res) => {
   const transaccion = req.params["transaccion"].toString();
   // eslint-disable-next-line max-len
