8242e8874a71ad27be71e17b882d6fd27427879c
paudpez14 <paudpez@hotmail.com>
2022-08-18 13:00:48 -0500

    Correción de función para crear info de todas las cryptos

--- functions/createCryptosInfo.js
+++ functions/createCryptosInfo.js
@@ -22,7 +22,7 @@ app.get(`/${process.env.API_KEY}`, async (req, res) => {
         if (err) throw err;
         for (const crypto of result) {
           const sqlStr2 = `
-              SELECT 
+              SELECT
               id,
               coin_id,
               symbol,
@@ -63,66 +63,72 @@ app.get(`/${process.env.API_KEY}`, async (req, res) => {
             `;
           pool.getConnection(function (error, connection) {
             if (error) throw error;
-            connection.query(sqlStr2, (err, result, fields) => {
+            connection.query(sqlStr2, async (err, result, fields) => {
               if (result) {
                 if (result[0].error === null) {
                   connection.destroy();
                   if (err) throw err;
-                  admin
+                  const query = admin
                     .firestore()
                     .collection("cryptos_info")
-                    .doc(crypto["name"])
-                    .create({
-                      coin_id: result[0]["name"],
-                      symbol: result[0]["symbol"],
-                      name: result[0]["name"],
-                      description: "",
-                      homepage: result[0]["homepage"],
-                      blockchain_site: result[0]["blockchain_site"],
-                      twitter_screenname: result[0]["twitter_screenname"],
-                      image_thumb: result[0]["image_thumb"],
-                      image_small: result[0]["image_small"],
-                      image_large: result[0]["image_large"],
-                      sentiment_votes_up_percentage: Number(
-                        result[0]["sentiment_votes_up_percentage"]
-                      ),
-                      sentiment_votes_down_percentage: Number(
-                        result[0]["sentiment_votes_down_percentage"]
-                      ),
-                      coingecko_rank: result[0]["coingecko_rank"],
-                      coingecko_score: Number(result[0]["coingecko_score"]),
-                      developer_score: Number(result[0]["developer_score"]),
-                      community_score: Number(result[0]["community_score"]),
-                      liquidity_score: Number(result[0]["liquidity_score"]),
-                      public_interest_score: Number(
-                        result[0]["public_interest_score"]
-                      ),
-                      ath_change_percentage: Number(
-                        result[0]["ath_change_percentage"]
-                      ),
-                      market_cap: result[0]["market_cap"],
-                      market_cap_rank: Number(result[0]["market_cap_rank"]),
-                      total_volume: result[0]["total_volume"],
-                      price_change_percentage_24h: Number(
-                        result[0]["price_change_percentage_24h"]
-                      ),
-                      price_change_percentage_7d: Number(
-                        result[0]["price_change_percentage_7d"]
-                      ),
-                      price_change_percentage_30d: Number(
-                        result[0]["price_change_percentage_7d"]
-                      ),
-                      price_change_percentage_200d: Number(
-                        result[0]["price_change_percentage_200d"]
-                      ),
-                      price_change_percentage_1y: Number(
-                        result[0]["price_change_percentage_1y"]
-                      ),
-                      total_supply: result[0]["total_supply"],
-                      max_supply: result[0]["max_supply"],
-                      circulating_supply: result[0]["circulating_supply"],
-                      last_updated: Date(result[0]["last_updated"]),
-                    });
+                    .doc(crypto["name"]);
+                  if (!((await query.get()).exists)) {
+                    admin
+                      .firestore()
+                      .collection("cryptos_info")
+                      .doc(crypto["name"])
+                      .create({
+                        coin_id: result[0]["name"],
+                        symbol: result[0]["symbol"],
+                        name: result[0]["name"],
+                        description: "",
+                        homepage: result[0]["homepage"],
+                        blockchain_site: result[0]["blockchain_site"],
+                        twitter_screenname: result[0]["twitter_screenname"],
+                        image_thumb: result[0]["image_thumb"],
+                        image_small: result[0]["image_small"],
+                        image_large: result[0]["image_large"],
+                        sentiment_votes_up_percentage: Number(
+                          result[0]["sentiment_votes_up_percentage"]
+                        ),
+                        sentiment_votes_down_percentage: Number(
+                          result[0]["sentiment_votes_down_percentage"]
+                        ),
+                        coingecko_rank: result[0]["coingecko_rank"],
+                        coingecko_score: Number(result[0]["coingecko_score"]),
+                        developer_score: Number(result[0]["developer_score"]),
+                        community_score: Number(result[0]["community_score"]),
+                        liquidity_score: Number(result[0]["liquidity_score"]),
+                        public_interest_score: Number(
+                          result[0]["public_interest_score"]
+                        ),
+                        ath_change_percentage: Number(
+                          result[0]["ath_change_percentage"]
+                        ),
+                        market_cap: result[0]["market_cap"],
+                        market_cap_rank: Number(result[0]["market_cap_rank"]),
+                        total_volume: result[0]["total_volume"],
+                        price_change_percentage_24h: Number(
+                          result[0]["price_change_percentage_24h"]
+                        ),
+                        price_change_percentage_7d: Number(
+                          result[0]["price_change_percentage_7d"]
+                        ),
+                        price_change_percentage_30d: Number(
+                          result[0]["price_change_percentage_7d"]
+                        ),
+                        price_change_percentage_200d: Number(
+                          result[0]["price_change_percentage_200d"]
+                        ),
+                        price_change_percentage_1y: Number(
+                          result[0]["price_change_percentage_1y"]
+                        ),
+                        total_supply: result[0]["total_supply"],
+                        max_supply: result[0]["max_supply"],
+                        circulating_supply: result[0]["circulating_supply"],
+                        last_updated: Date(result[0]["last_updated"]),
+                      });
+                  }
                 }
               } else {
                 connection.destroy();
