e05e65f6f72b281378f9735aaabd61fe842d77b4
Dan <danneira@Dans-Mac-mini.local>
2022-09-02 15:52:02 -0500

    Se ha implementado el sistema de envio de correos y con un archivo de pdf

--- functions/reports.js
+++ functions/reports.js
@@ -1,7 +1,9 @@
+/* eslint-disable max-len */
 const express = require("express");
 const cors = require("cors");
 const ejs = require("ejs");
-const fs = require("fs");
+// const fs = require("fs");
+const nodemailer = require("nodemailer");
 const pdf = require("html-pdf");
 const path = require("path");
 const app = express();
@@ -19,6 +21,13 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
   const nombre = req.body.name;
   const fecha = req.body.date;
   const person = {nombre: nombre, fecha: fecha};
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // true for 465, false for other ports
+    auth: {
+      user: "pauldelpezo@gmail.com", // generated ethereal user
+      pass: "vhbxnuouduqxexsl", // generated ethereal password
+    },
+  });
   // eslint-disable-next-line max-len
   ejs.renderFile(path.join(__dirname, "./views/", "reporte.ejs"), person, (err, result) => {
     if (err) {
@@ -34,14 +43,38 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
           "height": "20mm",
         },
       };
-      try {
-        // eslint-disable-next-line max-len
-        fs.mkdir(path.join(__dirname, "./public/reports/"), {recursive: true}, (err) => {
-          if (err) throw err;
+      pdf.create(result, options).toBuffer(function(err, buffer) {
+        console.log("This is a buffer:", Buffer.isBuffer(buffer));
+        console.log("Bandera");
+        const mailOptions = {
+          from: "pauldelpezo@gmail.com",
+          to: "paul@invrtir.com",
+          subject: "Prueba",
+          text: "Prueba",
+          attachments: [
+            {
+              // eslint-disable-next-line new-cap
+              content: Buffer.from(buffer),
+              contentType: "application/pdf",
+            },
+          ]
+          ,
+        };
+        transporter.sendMail(mailOptions, (err, response) => {
+          if (err) {
+            console.error("there was an error: ", err);
+            res.status(401).json(err);
+          } else {
+            // console.log('here is the res: ', response);
+            res.status(200).json("recovery email sent");
+          }
         });
-        fs.rm(path.join(__dirname, "./public/reports/", "report.pdf"), ()=>{
+      });
+      /*
+      try {
+        fs.rm("/workspace/reports/report.pdf", ()=>{
           // eslint-disable-next-line max-len
-          pdf.create(result, options).toFile("./public/reports/report.pdf", function(err, data) {
+          pdf.create(result, options).toFile("/workspace/reports/report.pdf", function(err, data) {
             if (err) {
               res.send(err);
             } else {
@@ -54,8 +87,8 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
           });
         });
       } catch (e) {
-        // eslint-disable-next-line max-len
-        pdf.create(result, options).toFile("./public/reports/report.pdf", function(err, data) {
+        // eslint-disable-next-line max-len, max-len, max-len
+        pdf.create(result, options).toFile("/workspace/reports/report.pdf", function(err, data) {
           if (err) {
             res.send(err);
           } else {
@@ -68,6 +101,7 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
         });
         return "Archivo Enviado";
       }
+      */
     }
   });
 });
