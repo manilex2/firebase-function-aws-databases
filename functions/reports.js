0fa462e4c91adea9bef1913c80d32fffb6d6ff1e
Dan <danneira@Dans-Mac-mini.local>
2022-09-02 16:59:44 -0500

    Mejoras en el template de reportes

--- functions/reports.js
+++ functions/reports.js
@@ -10,17 +10,36 @@ const app = express();
 app.use(cors({origin: true}));
 app.set("view engine", "ejs");
 app.get(`/${process.env.API_KEY}`, (req, res) => {
-  const nombre = "req.body.name";
-  const fecha = "req.body.date";
-  const data = {nombre: nombre, fecha: fecha};
-  console.log(data);
+  const nombre = "Paul Navarrete";
+  const fecha = "14-02-2022";
+  const cargo = "Cargo";
+  const unidad = "Unidad";
+  const mes = "Mes";
+  const totalHoras = "TotalHoras";
+  const fechas = ["14-07-2022", "14-08-2022", "14-09-2022"];
+  const horasEntrada = ["18:20", "18:30", "18:40"];
+  const horasSalida = ["20:20", "20:30", "21:20"];
+  const totalHorasTrabajadas = ["18:20", "20:20", "21:20"];
+  const horasExtras = ["18:20", "20:20", "21:20"];
+  const horasFaltantes = ["18:20", "20:20", "21:20"];
+  const data = {nombre: nombre, fecha: fecha, cargo: cargo, unidad: unidad, mes: mes, totalHoras: totalHoras, fechas: fechas, horasEntrada: horasEntrada, horasSalida: horasSalida, totalHorasTrabajadas: totalHorasTrabajadas, horasExtras: horasExtras, horasFaltantes: horasFaltantes};
   res.render("reporte", data);
 });
 app.post(`/${process.env.API_KEY}`, (req, res) => {
   res.set("Content-Type", "application/json");
   const nombre = req.body.name;
   const fecha = req.body.date;
-  const person = {nombre: nombre, fecha: fecha};
+  const cargo = req.body.cargo;
+  const unidad = req.body.unidad;
+  const mes = req.body.mes;
+  const totalHoras = req.body.totalHoras;
+  const fechas = req.body.fechas;
+  const horasEntrada = req.body.horasEntrada;
+  const horasSalida = req.body.horasSalida;
+  const totalHorasTrabajadas = ["18:20", "20:20", "21:20"];
+  const horasExtras = ["18:20", "20:20", "21:20"];
+  const horasFaltantes = ["18:20", "20:20", "21:20"];
+  const person = {nombre: nombre, fecha: fecha, cargo: cargo, unidad: unidad, mes: mes, totalHoras: totalHoras, fechas: fechas, horasEntrada: horasEntrada, horasSalida: horasSalida, totalHorasTrabajadas: totalHorasTrabajadas, horasExtras: horasExtras, horasFaltantes: horasFaltantes};
   const transporter = nodemailer.createTransport({
     service: "gmail", // true for 465, false for other ports
     auth: {
