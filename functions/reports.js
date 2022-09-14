7125c34c6ff534d0b9717f7113345c18d5bf4a09
Dan <danneira@Dans-Mac-mini.local>
2022-09-14 13:31:34 -0500

    Se ha terminado un arreglo final del html para general el reporte de Nailbox, quedan por corregir

--- functions/reports.js
+++ functions/reports.js
@@ -29,8 +29,6 @@ app.get(`/${process.env.API_KEY}`, (req, res) => {
 });
 app.post(`/${process.env.API_KEY}`, (req, res) => {
   res.set("Content-Type", "application/json");
-  console.log("Bandera");
-  console.log(req.body);
   const nombre = req.body.nombre;
   const cedula = req.body.cedula;
   const cargo = req.body.cargo;
@@ -45,7 +43,6 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
   const horasFaltantes = req.body.horasfaltantes;
   const person = {nombre: nombre, cedula: cedula, cargo: cargo, unidad: unidad, mes: mes, totalHoras: totalHoras, fechas: fechas, horasEntrada: horasEntrada, horasSalida: horasSalida, totalHorasTrabajadas: totalHorasTrabajadas, horasExtras: horasExtras, horasFaltantes: horasFaltantes};
   console.log(person);
-
   const transporter = nodemailer.createTransport({
     service: "gmail", // true for 465, false for other ports
     auth: {
@@ -53,57 +50,6 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
       pass: `${process.env.PASSWORD_GMAIL}`, // generated ethereal password
     },
   });
-  const template = fs.readFileSync(path.join(__dirname, "./views/", "reporte.ejs"), "utf8");
-  const content = ejs.render(template, person);
-  jsreport.init().then(() => {
-    return jsreport.render({
-      template: {
-        content: content,
-        engine: "handlebars",
-        recipe: "chrome-pdf",
-      },
-    }).then((resp) => {
-      // prints pdf with headline Hello world
-      console.log(resp);
-    });
-  }).catch((e) => {
-    console.error(e);
-  });
-  /*
-  const options = {
-    format: "A4",
-    preferCSSPageSize: true,
-  };
-  const file = {
-    content: content,
-  };
-  pdf.generatePdf(file, options).then((pdfBuffer) => {
-    const mailOptions = {
-      from: `${process.env.EMAIL_GMAIL}`,
-      to: `${process.env.EMAIL_DESTINY_TEST_EMAIL}`,
-      subject: `Reporte de horas trabajada de: ${nombre} | ${mes}`,
-      text: "Reporte Generado desde la aplicación de Nailbox",
-      attachments: [
-        {
-          // eslint-disable-next-line new-cap
-          filename: `reporte_${nombre}_${mes}`,
-          content: Buffer.from(pdfBuffer),
-          contentType: "application/pdf",
-        },
-      ]
-      ,
-    };
-    transporter.sendMail(mailOptions, (err, response) => {
-      if (err) {
-        console.error("there was an error: ", err);
-        res.status(401).json(err);
-      } else {
-        // console.log('here is the res: ', response);
-        res.status(200).json("recovery email sent");
-      }
-    });
-  });
-  /*
   // eslint-disable-next-line max-len
   ejs.renderFile(path.join(__dirname, "./views/", "reporte.ejs"), person, (err, result) => {
     if (err) {
@@ -119,7 +65,7 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
       pdf.generatePdf(file, options).then((pdfBuffer) => {
         const mailOptions = {
           from: `${process.env.EMAIL_GMAIL}`,
-          to: `${process.env.EMAIL_DESTINY_TEST_EMAIL}`,
+          to: `${process.env.EMAIL_DESTINY_GMAIL}`,
           subject: `Reporte de horas trabajada de: ${nombre} | ${mes}`,
           text: "Reporte Generado desde la aplicación de Nailbox",
           attachments: [
@@ -144,7 +90,5 @@ app.post(`/${process.env.API_KEY}`, (req, res) => {
       });
     }
   });
-  */
 });
-
 module.exports = app;
